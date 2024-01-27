0fa26d546536a8dd1d4e4474b53bc44f75b58f17
twhit27 <145574326+twhit27@users.noreply.github.com>
2024-01-27 15:41:12 -0500

    Update Internet.cs
    added the temp main I used to test some of server graph, by no means comprehensive testing!

--- Internet.cs
+++ Internet.cs
@@ -394,3 +394,59 @@ public class WebGraph
     }
 
 }
+class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+
+            Console.WriteLine("Adjacency Matrix Implemention");
+
+            DirectedGraph<char> H = new DirectedGraph<char>(7);
+
+            for (i = 0; i < 7; i++)
+                H.AddVertex((char)(i + 'a'));
+
+            H.PrintVertices();
+
+            for (i = 0; i < 7; i += 2)
+                for (j = 0; j < 7; j += 3)
+                {
+                    H.AddEdge((char)(i + 'a'), (char)(j + 'a'), 10);
+                }
+
+            H.PrintEdges();
+            Console.ReadKey();
+
+            H.RemoveVertex('c');
+            H.RemoveVertex('f');
+
+            H.PrintVertices();
+            H.PrintEdges();
+            Console.ReadKey();
+
+            DirectedGraph<int> G = new DirectedGraph<int>(10);
+
+            for (i = 0; i < 7; i++)
+                G.AddVertex(i);
+
+            G.PrintVertices();
+
+            G.AddEdge(0, 1, 0);
+            G.AddEdge(1, 3, 0);
+            G.AddEdge(1, 4, 0);
+            G.AddEdge(3, 0, 0);
+            G.AddEdge(1, 2, 0);
+            G.AddEdge(2, 5, 0);
+            G.AddEdge(5, 6, 0);
+
+            G.PrintEdges();
+            Console.ReadKey();
+
+            G.DepthFirstSearch();
+            Console.ReadKey();
+
+            G.BreadthFirstSearch();
+            Console.ReadKey();
+        }
+    }
